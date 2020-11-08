85054945431cd82412e76831c6e0671c3034d5b5
lukasdebeerst <lukas.debeerst@icloud.com>
2020-11-08 15:22:28 +0100

    image update

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -12,19 +12,19 @@ function BlogPost (props) {
   const {_rawBody, authors, categories, title, mainImage, publishedAt} = props
   return (
     <article className={styles.root}>
-      {mainImage && mainImage.asset && (
+      {/* {mainImage && mainImage.asset && (
         <div className={styles.mainImage}>
           <img
             src={imageUrlFor(buildImageObj(mainImage))
-              .width(1200)
-              .height(Math.floor((9 / 16) * 1200))
+              .width(500)
+              .height(Math.floor((9 / 16) * 500))
               .fit('crop')
               .auto('format')
               .url()}
             alt={mainImage.alt}
           />
         </div>
-      )}
+      )} */}
       <Container>
         <div className={styles.grid}>
           <div className={styles.mainContent}>
